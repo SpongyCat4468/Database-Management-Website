af616f17804e155fefc13dc5f5f151c1efb39ce0
SpongyCat <130354790+SpongyCat4468@users.noreply.github.com>
2025-08-11 22:52:08 +0800

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,39 @@
+""" 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process 
+.\venv\Scripts\Activate.ps1
+"""
+import threading
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from discord_bot import bot
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+commands_text = """
+/calc
+/create_thread <channel> <name> <content>
+/delete_thread <channel> <name>
+/edit_thread <channel> <old_title> <new_title>
+/list_thread <channel>
+/random_thread <channel>
+/search_thread <channel> <name>
+/trend <channel>
+"""
+
+@app.route("/")
+def index():
+     return render_template("index.html", commands=commands_text)
+
+
+def start_bot():
+    bot.run(os.getenv("TOKEN"))
+
+
+if __name__ == "__main__":
+    threading.Thread(target=start_bot, daemon=True).start()
+    app.run(use_reloader=False)
+    
+    
